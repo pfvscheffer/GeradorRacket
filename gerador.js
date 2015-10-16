a2f6f38e6218d59bd17323948d2899a9d9d32000
pfvscheffer <pedro.felipe.vergo@gmail.com>
2015-10-16 00:33:13 -0300

    Structs prontos

--- gerador.js
+++ gerador.js
@@ -1,3 +1,30 @@
+function Parametro(string) {
+	var palavras = string.trim().split(/\s+/);
+	this.nome = palavras[0];
+	this.tipo = toPascalCase(palavras[1]);
+}
+
+function FuncoesStruct(nome, params) {
+	var lNome = toCamelCase(nome);
+	this.construtor = '(make-' + lNome + ' ' + declaraParametros(params) + ')';
+	this.questao = '(' + lNome + '? ' + lNome[0] + ')';
+	this.seletores = makeSeletores(nome, params);
+}
+
+function selecionar() {
+	this.select();
+}
+
+function makeSeletores(nome, params) {
+	var seletores = [];
+
+	for (var i = 0, len = params.length; i < len; i++) {
+		seletores.push('(' + nome + '-' + params[i].nome + ')');
+	}
+
+	return seletores;
+}
+
 function ajustarAltura(textarea) {
 	textarea.rows = textarea.value.split(/\r?\n/).length;
 }
@@ -7,27 +34,79 @@ textosSaida = document.getElementsByClassName("txtSaida");
 (function () {
 	for (var i = 0; i < textosSaida.length; i++) {
 		ajustarAltura(textosSaida[i]);
-		/* textosSaida[i].addEventListener("input", function() {
-			this.rows = this.value.split(/\r?\n/);
-		}); */
 	}
 })();
 
 function criarStruct() {
 	var nome = document.getElementById("txtStructNome").value;
-	var params = document.getElementById("txtStructParam").value.split(/,/);
+	var params = gerarParametros(document.getElementById("txtStructParam").value);
+	var funcoes = new FuncoesStruct(nome, params);
+
 	var texto = '(define-struct ' + toCamelCase(nome);
-	texto += ' (' + declaraParametros(params) + '))';
-	
-	document.getElementById("txtStructSaida").value = texto;
+	texto += ' (' + declaraParametros(params) + '))\n';
+	texto += ';; Elemento do conjunto ' + toPascalCase(nome) + '.\n';
+	texto += ';; (make-' + toCamelCase(nome) + ' ' + makeParametros(params) + ')';
+
+	if (params.length > 0) {
+		texto += ' onde:\n';
+		texto += descreveParams(params);
+	}
+
+	var txtSaida = document.getElementById("txtStructSaida");
+	txtSaida.value = texto;
+	ajustarAltura(txtSaida);
+
+	var divSaida = document.getElementById('metodosStruct');
+	divSaida.innerHTML = '';
+
+	divSaida.appendChild(novaTextArea(funcoes.construtor));
+	divSaida.appendChild(novaTextArea(funcoes.questao));
+
+	for (var i = 0, len = funcoes.seletores.length; i < len; i++) {
+		divSaida.appendChild(novaTextArea(funcoes.seletores[i]));
+	}
 }
 
-function declaraParametros(strArr) {
-	var arrParams = [];
-	for (var i = 0; i < strArr.length; i++) {
-		arrParams.push(strArr[i].trim().split(/\s+/)[0]);
+function novaTextArea(string) {
+	var novaArea = document.createElement('textarea');
+	novaArea.className = 'txtSaida';
+	novaArea.rows = 1;
+	novaArea.value = string;
+	novaArea.addEventListener('click', selecionar);
+	return novaArea;
+}
+
+function descreveParams(params) {
+	var linhas = [];
+	for (var i = 0, len = params.length; i < len; i++) {
+		linhas.push(';; 1' + params[i].nome + ': ' + params[i].tipo);
 	}
-	return arrParams.join(" ");
+	return linhas.join('\n');
+}
+
+function gerarParametros(textoParams) {
+	var arr = textoParams.split(/,/);
+	var saida = [];
+	for (var i = 0, len = arr.length; i < len; i++) {
+		saida.push(new Parametro(arr[i]));
+	}
+	return saida;
+}
+
+function declaraParametros(parametros) {
+	var nomes = [];
+	for (var i = 0, len = parametros.length; i < len; i++) {
+		nomes.push(parametros[i].nome);
+	}
+	return nomes.join(" ");
+}
+
+function makeParametros(parametros) {
+	var nomes = [];
+	for (var i = 0, len = parametros.length; i < len; i++) {
+		nomes.push(1 + parametros[i].nome);
+	}
+	return nomes.join(" ");
 }
 
 function nomesParametros(arrayParametros) {
@@ -50,3 +129,9 @@ function toPascalCase(string) {
 }
 
 document.getElementById("btnStruct").addEventListener("click", criarStruct);
+(function () {
+	var saidas = document.getElementsByClassName('txtSaida');
+	for (var i = 0, len = saidas.length; i < len; i++) {
+		saidas[i].addEventListener('click', selecionar);
+	}
+})();
